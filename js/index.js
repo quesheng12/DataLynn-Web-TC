e0861b2c01c84c678985a95de431ea65bb27712e
Zhao Yiran <ejanquesheng@gmail.com>
2023-04-20 08:45:54 -0400

    scroll

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    // $("body").quietflow({
+    //     theme: "simpleGradient",
+    //     primary: "#B066FE",
+    //     accent: "#63E2FF"
+    // })
+
+    // $(window).on('load resize', function () {
+    //     var $thisnav = $('.current-menu-item').offset().left;
+    //     $('.menu-item').hover(function () {
+    //         var $left = $(this).offset().left - $thisnav;
+    //         var $width = $(this).outerWidth();
+    //         var $start = 0;
+    //         $('.wee').css({'left': $left, 'width': $width});
+    //     }, function () {
+    //         var $initwidth = $('.current-menu-item').width();
+    //         $('.wee').css({'left': '0', 'width': $initwidth});
+    //     });
+    // });
+});
